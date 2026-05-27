306cc867f1e500670cad7ad132f1bed83e85ea7f
Vidhyashankar Balasubramaniyan <vbalasubramaniyan@atlassian.com>
2026-05-27 11:07:21 -0700

    Create clean SwiftUI iOS app

---
+++ ios-ssh-app/ios_ssh_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_ssh_appApp.swift
+//  ios-ssh-app
+//
+//  Created by Vidhyashankar Balasubramaniyan on 5/27/26.
+//
+
+import SwiftUI
+
+@main
+struct ios_ssh_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
